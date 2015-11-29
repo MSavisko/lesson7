5b6cff964e4eea3e9e0d1feb8dc3d63d0122677d
Maksym Savisko <stowyn@gmail.com>
2015-11-29 18:01:42 +0200

    Task #2. Init TableView

---
+++ task2/Shopping List/Shopping List/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Shopping List
+//
+//  Created by Maksym Savisko on 11/29/15.
+//  Copyright © 2015 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
---
+++ task2/Shopping List/Shopping List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shopping List
+//
+//  Created by Maksym Savisko on 11/29/15.
+//  Copyright © 2015 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
