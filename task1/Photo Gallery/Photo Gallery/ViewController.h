e0243920944e7ca9e389d64c11d23c42c12358b4
Maksym Savisko <stowyn@gmail.com>
2015-11-29 23:03:33 +0200

    Task #1. Init

---
+++ task1/Photo Gallery/Photo Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photo Gallery
+//
+//  Created by Maksym Savisko on 11/29/15.
+//  Copyright © 2015 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ task1/Photo Gallery/Photo Gallery/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Photo Gallery
+//
+//  Created by Maksym Savisko on 11/29/15.
+//  Copyright © 2015 Maksym Savisko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic) NSMutableArray * images;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
